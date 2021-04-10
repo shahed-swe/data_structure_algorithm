b399c36c02ffdef6250b558a496540ea05dec4fd
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-04-10 23:30:48 +0600

    new commit

--- random_number_generator.py
+++ random_number_generator.py
@@ -1,10 +1,10 @@
 import random
 new_list = []
 while True:
-    num = random.randint(0,4000)
+    num = random.randint(0,10000)
     if num not in new_list:
         new_list.append(num)
-        if len(new_list) == 3999:
+        if len(new_list) == 9999:
             break
         else:
             pass
